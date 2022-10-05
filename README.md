41ddd41cf9386bbee91e3639f7b5518eac120679
Sarathkumar <27sarathkumar.s@gmail.com>
2022-10-05 10:08:21 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyNetworkManager
+
+A description of this package.
---
+++ Sources/MyNetworkManager/MyNetworkManager.swift
@@ -0,0 +1,6 @@
+public struct MyNetworkManager {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
